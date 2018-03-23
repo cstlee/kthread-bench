54330dc0240440745955b146bf665fbcca48b4f6
Collin Lee <cstlee@cs.stanford.edu>
2018-03-23 15:30:23 -0700

    Add latency measurement and output

--- src/dispatch.cc
+++ src/dispatch.cc
@@ -13,15 +13,38 @@
 #include <cstdio>
 
 #include "control.h"
+#include "PerfUtils/Cycles.h"
+
+using PerfUtils::Cycles;
 
 int main(int argc, char *argv[])
 {
-    if (argc < 1)
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: dispatch\n");
-        return 0;
+        fprintf(stderr, "Usage: dispatch <num_workers> <num_samples>\n");
+        return 1;
     }
 
+    int num_workers = atoi(argv[1]);
+    int num_samples = atoi(argv[2]);
+
+    if (num_workers < 1)
+    {
+        fprintf(stderr, "Cannot support less than 1 worker. "
+                        "%d workers requested.\n",
+                num_workers);
+        return 1;
+    }
+
+    if (num_workers > MAX_WORKER_COUNT)
+    {
+        fprintf(stderr, "Cannot support more than %d workers. "
+                        "%d workers requested.\n",
+                MAX_WORKER_COUNT, num_workers);
+        return 1;
+    }
+
+    uint64_t samples[num_samples];
     size_t mem_size = sizeof(struct ControlTable);
     struct ControlTable *control;
 
@@ -29,43 +52,69 @@ int main(int argc, char *argv[])
     if (fd == -1)
     {
         fprintf(stderr, "Could not open shared memory region. (%s)\n", strerror(errno));
+        return 1;
     }
 
     if (ftruncate(fd, mem_size) == -1)
     {
         fprintf(stderr, "Could not truncate memory region. (%s)\n", strerror(errno));
+        return 1;
     }
 
     control = (struct ControlTable *)mmap(NULL, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (control == (struct ControlTable *)-1)
     {
         fprintf(stderr, "Could not mmap memory region. (%s)\n", strerror(errno));
+        return 1;
     }
 
     memset(control, 0, mem_size);
-    printf("Ready\n");
 
-    while (1)
+    // Wait for requested number of workers to spin up.
+    while (control->nextWorkerId.load() < num_workers)
+        ;
+
+    for (int i = 0; i < num_samples; ++i)
     {
-        usleep(100);
-        if (control->nextWorkerId.load() < 1)
-        {
-            continue;
-        }
+        // Race Condition; Slow down the dispatch to try to make sure the worker
+        // has time to go back to sleep before we try to signal.
+        Cycles::sleep(100);
 
         int workerId = rand() % control->nextWorkerId.load();
+        int err = 0;
+        uint64_t startTSC = 0;
+        uint64_t endTSC = 0;
 
         control->controlBlock[workerId].futex = WORKER_WAKE;
-        syscall(SYS_futex, &control->controlBlock[workerId].futex, FUTEX_WAKE, 1, NULL, NULL, 0);
-
-        while (control->controlBlock[workerId].val.load() == MAX_WORKER_COUNT)
+        startTSC = Cycles::rdtsc();
+        err = syscall(SYS_futex, &control->controlBlock[workerId].futex, FUTEX_WAKE, 1, NULL, NULL, 0);
+        if (err == -1)
         {
-            usleep(10);
-            printf("%d %d\n", workerId, control->nextWorkerId.load());
+            fprintf(stderr, "Error while trying to wake futex. (%s)\n", strerror(errno));
+            return 1;
         }
 
-        printf("val: %lu\n", control->controlBlock[workerId].val.load());
-        control->controlBlock[workerId].val.store(MAX_WORKER_COUNT);
+        // Wait for worker to wake up and report latency.
+        while (control->controlBlock[workerId].val.load() == 0)
+            ;
+        endTSC = control->controlBlock[workerId].val.load();
+        control->controlBlock[workerId].val.store(0);
+
+        samples[i] = Cycles::toNanoseconds(endTSC - startTSC);
+    }
+
+    // Print samples
+    for (int i = 0; i < num_samples; ++i)
+    {
+        printf("%7lu,", samples[i]);
+        if (i % 10 == 9)
+        {
+            printf("\n");
+        }
+    }
+    if (num_samples % 10 != 0)
+    {
+        printf("\n");
     }
 
     return 0;
--- src/worker.cc
+++ src/worker.cc
@@ -12,13 +12,16 @@
 #include <cstdio>
 
 #include "control.h"
+#include "PerfUtils/Cycles.h"
+
+using PerfUtils::Cycles;
 
 int main(int argc, char *argv[])
 {
     if (argc < 1)
     {
         fprintf(stderr, "Usage: worker\n");
-        return 0;
+        return 1;
     }
 
     size_t mem_size = sizeof(struct ControlTable);
@@ -27,37 +30,45 @@ int main(int argc, char *argv[])
     int fd = shm_open("/kthread-bench-ctrl", O_RDWR, S_IRWXU);
     if (fd == -1)
     {
-        fprintf(stderr, "Could not open shared memory region. (%s)\n", strerror(errno));
+        fprintf(stderr, "Could not open shared memory region. [%d](%s)\n", errno, strerror(errno));
+        return 1;
     }
 
     if (ftruncate(fd, mem_size) == -1)
     {
-        fprintf(stderr, "Could not truncate memory region. (%s)\n", strerror(errno));
+        fprintf(stderr, "Could not truncate memory region. [%d](%s)\n", errno, strerror(errno));
+        return 1;
     }
 
     control = (struct ControlTable *)mmap(NULL, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (control == (struct ControlTable *)-1)
     {
-        fprintf(stderr, "Could not mmap memory region. (%s)\n", strerror(errno));
+        fprintf(stderr, "Could not mmap memory region. [%d](%s)\n", errno, strerror(errno));
+        return 1;
     }
 
     int workerId = control->nextWorkerId.fetch_add(1);
 
     if (workerId >= MAX_WORKER_COUNT)
     {
-        printf("Aborting... too many workers.\n");
-        return 0;
+        fprintf(stderr, "Aborting... too many workers.\n");
+        return 1;
     }
 
-    printf("WorkerID %d\n", workerId);
 
     while (1)
     {
-        printf("blocking\n");
-        syscall(SYS_futex, &control->controlBlock[workerId].futex, FUTEX_WAIT, WORKER_SLEEP, NULL, NULL, 0);
-        printf("now awake\n");
+        int err = 0;
+        uint64_t endTSC = 0;
+        err = syscall(SYS_futex, &control->controlBlock[workerId].futex, FUTEX_WAIT, WORKER_SLEEP, NULL, NULL, 0);
+        endTSC = Cycles::rdtsc();
+        if (err == -1)
+        {
+            fprintf(stderr, "Error while trying to wait on futex. [%d](%s)\n", errno, strerror(errno));
+            return 1;
+        }
         control->controlBlock[workerId].futex = WORKER_SLEEP;
-        control->controlBlock[workerId].val.store(workerId);
+        control->controlBlock[workerId].val.store(endTSC);
     }
 
     return 0;
